8501e04666247331a1b97d2a973f3f3323b0ecb0
Ali Selcuk <maselcuk@gmail.com>
2017-05-14 01:21:07 +0300

    fix

--- src/Domains/Droplet/Jobs/CreateDropletPathsJob.php
+++ src/Domains/Droplet/Jobs/CreateDropletPathsJob.php
@@ -17,7 +17,7 @@ class CreateDropletPathsJob
     
     public function handle(Filesystem $filesystem)
     {
-        $this->model->path("droplets/{$this->model->vendor}/{$type}s/{$this->model->name}");
+        $this->model->path("droplets/{$this->model->path}");
         
         $path = base_path($this->model->path());
         $filesystem->makeDirectory($path, 0755, true, true);
