328ac4ffa777d146c6df98f31ed3c7692f477d4a
Ali Selcuk <maselcuk@gmail.com>
2017-06-14 07:39:16 +0300

    path with nucleus completed: "create dns zone on server"

--- src/Domains/Entry/EntryObserver.php
+++ src/Domains/Entry/EntryObserver.php
@@ -1,5 +1,6 @@
 <?php namespace SuperV\Platform\Domains\Entry;
 
+use SuperV\Nucleus\Domains\Entry\Entry;
 use SuperV\Platform\Contracts\Dispatcher;
 
 class EntryObserver
@@ -11,47 +12,58 @@ class EntryObserver
         $this->events = $events;
     }
 
-    public function creating(EntryModel $entry)
+    public function creating(Entry $entry)
+    {
+        $entry->creating = true;
+    }
+
+    public function created(Entry $entry)
     {
     }
 
-    public function created(EntryModel $entry)
+    public function updating(Entry $entry)
     {
     }
 
-    public function updating(EntryModel $entry)
+    public function updated(Entry $entry)
     {
     }
 
-    public function updated(EntryModel $entry)
-    {
-    }
-
-    public function updatedMultiple(EntryModel $entry)
-    {
-        $entry->flushCache();
-    }
-
-    public function saving(EntryModel $entry)
-    {
-        return true;
-    }
-
-    public function saved(EntryModel $entry)
-    {
-    }
-
-    public function deleting(EntryModel $entry)
-    {
-        return true;
-    }
-
-    public function deleted(EntryModel $entry)
+    public function updatedMultiple(Entry $entry)
     {
         $entry->flushCache();
     }
 
-    public function deletedMultiple(EntryModel $entry)
+    public function saving($entry)
+    {
+        return true;
+    }
+
+    public function saved(Entry $entry)
+    {
+        if ($entry->creating) {
+            return;
+        }
+        $class = get_class($entry->nucleo());
+
+        $observer = substr($class, 0, -5) . 'Observer';
+        $observing = class_exists($observer);
+        if ($observing) {
+            (new $observer)->saved($entry);
+        }
+    }
+
+    public function deleting(Entry $entry)
+    {
+        return true;
+    }
+
+    public function deleted(Entry $entry)
+    {
+        $entry->flushCache();
+    }
+
+    public function deletedMultiple(Entry $entry)
     {
         $entry->flushCache();
     }
