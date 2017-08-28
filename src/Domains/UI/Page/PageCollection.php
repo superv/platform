d525b57d958e55aaa2395c8ce4d70ded3a64df37
Ali Selcuk <maselcuk@gmail.com>
2017-08-28 05:15:22 +0300

    adds filter pages by model

--- src/Domains/UI/Page/PageCollection.php
+++ src/Domains/UI/Page/PageCollection.php
@@ -7,7 +7,7 @@ class PageCollection extends Collection
     public function byDroplet($slug)
     {
         $items = [];
-        foreach($this->items as $item) {
+        foreach ($this->items as $item) {
             if ($item->getDroplet()->getSlug() == $slug) {
                 $items[] = $item;
             }
@@ -15,4 +15,17 @@ class PageCollection extends Collection
 
         return new self($items);
     }
+
+    public function byModel($model)
+    {
+        $items = [];
+        foreach ($this->items as $item) {
+            if ($item->getModel() == $model) {
+                $items[$item->getVerb()] = $item;
+            }
+        }
+
+        return new self($items);
+    }
+
 }
