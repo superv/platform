92c0c2c36f293f621916bfc2370e32cd13f98694
Ali Selcuk <maselcuk@gmail.com>
2017-08-28 05:08:03 +0300

    all droplet types has its own stub for generating

--- src/Domains/Droplet/Jobs/WriteDropletFilesJob.php
+++ src/Domains/Droplet/Jobs/WriteDropletFilesJob.php
@@ -8,28 +8,29 @@ class WriteDropletFilesJob
 {
     /** @var \SuperV\Platform\Domains\Droplet\Model\DropletModel */
     private $model;
-    
+
     public function __construct(DropletModel $model)
     {
         $this->model = $model;
     }
-    
+
     public function handle(Filesystem $filesystem, Parser $parser)
     {
         $name = ucfirst(camel_case($this->model->getName()));
         $type = ucfirst(camel_case($this->model->type()));
-        
+
         $path = base_path($this->model->path());
-        
+
         // Droplet Class
         $dropletClass = "{$name}{$type}";
-        $content = $parser->parse($filesystem->get(base_path("vendor/superv/platform/resources/stubs/droplets/droplet.stub")),
+        $content = $parser->parse($filesystem->get(base_path("vendor/superv/platform/resources/stubs/droplets/{$type}.stub")),
             [
-                'class' => $dropletClass,
-                'model' => $this->model->toArray(),
+                'class'   => $dropletClass,
+                'extends' => ucwords($type),
+                'model'   => $this->model->toArray(),
             ]);
         $filesystem->put("{$path}/src/{$dropletClass}.php", $content);
-        
+
         // Service Provider
         $providerClass = "{$name}{$type}ServiceProvider";
         $content = $parser->parse($filesystem->get(base_path("vendor/superv/platform/resources/stubs/droplets/provider.stub")),
@@ -38,12 +39,12 @@ class WriteDropletFilesJob
                 'model' => $this->model->toArray(),
             ]);
         $filesystem->put("{$path}/src/{$providerClass}.php", $content);
-        
+
         // composer.json
         $content = $parser->parse($filesystem->get(base_path("vendor/superv/platform/resources/stubs/droplets/composer.stub")),
             [
-                'model' => $this->model->toArray(),
-                'prefix' => str_replace("\\", "\\\\", $this->model->namespace())
+                'model'  => $this->model->toArray(),
+                'prefix' => str_replace("\\", "\\\\", $this->model->namespace()),
             ]);
         $filesystem->put("{$path}/composer.json", $content);
     }
