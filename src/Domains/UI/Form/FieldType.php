d0d6b630a0045ca870f776146f64334fb034038a
Ali Selcuk <maselcuk@gmail.com>
2017-08-31 02:42:13 +0300

    relation field can be unmapped

--- src/Domains/UI/Form/FieldType.php
+++ src/Domains/UI/Form/FieldType.php
@@ -3,20 +3,20 @@
 namespace SuperV\Platform\Domains\UI\Form;
 
 use SuperV\Platform\Domains\Entry\EntryModel;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class FieldType
 {
     public static $fieldMap = [
-        'text'     => TextType::class,
+        'text' => TextType::class,
         'textarea' => TextareaType::class,
-        'integer'  => IntegerType::class,
-        'email'    => EmailType::class,
-        'choice'   => ChoiceType::class,
+        'integer' => IntegerType::class,
+        'email' => EmailType::class,
+        'choice' => ChoiceType::class,
         'relation' => ChoiceType::class,
     ];
 
@@ -72,12 +72,12 @@ class FieldType
     public function getOptions()
     {
         $options = [];
-        array_set($options, 'mapped', array_get($this->config, 'mapped', true));
-        array_set($options, 'required', array_get($this->config, 'required', false));
 
         array_set($options, 'rules', $this->rules);
         if ($this->type == 'relation') {
             array_set($options, 'mapped', true);
+            array_set($options, 'multiple', array_get($this->config, 'multiple', false));
+            array_set($options, 'expanded', array_get($this->config, 'expanded', false));
             if ($related = array_get($this->config, 'related')) {
                 if (method_exists($this->entry, $method = 'get'.studly_case($this->field).'Options')) {
                     $choices = $this->entry->{$method}()->pluck('id', 'name')->toArray();
@@ -89,11 +89,8 @@ class FieldType
             }
         }
 
-        if ($this->type == 'relation') {
-            array_set($options, 'multiple', array_get($this->config, 'multiple', false));
-            array_set($options, 'expanded', array_get($this->config, 'expanded', false));
-        }
-
+        array_set($options, 'mapped', array_get($this->config, 'mapped', true));
+        array_set($options, 'required', array_get($this->config, 'required', false));
         array_set($options, 'translation_domain', false);
 
         return $options;
