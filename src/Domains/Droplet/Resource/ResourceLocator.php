8c66f35f09f15413032c7aac9298fd843d24000f
Ali Selcuk <maselcuk@gmail.com>
2017-09-05 15:27:14 +0300

    meh

--- src/Domains/Droplet/Resource/ResourceLocator.php
+++ src/Domains/Droplet/Resource/ResourceLocator.php
@@ -18,12 +18,17 @@ class ResourceLocator
 
     public function locate($namespace, $type = null)
     {
+        if (str_is('superv::*', $namespace)) {
+            $namespace = str_replace('superv::', 'superv.platform::', $namespace);
+        }
+
         if (! str_is('*::*', $namespace)) {
             return null;
         }
         list($droplet, $resource) = explode('::', $namespace);
         $droplet = $this->droplets->get($droplet);
 
+        $resource = str_replace('.', '/', $resource);
         $location = base_path($droplet->getPath().'/resources/'.str_plural($type).'/'.$resource.'.'.$type);
 
         return $location;
--- src/Domains/Task/Table/TaskTableBuilder.php
+++ src/Domains/Task/Table/TaskTableBuilder.php
@@ -3,23 +3,24 @@
 namespace SuperV\Platform\Domains\Task\Table;
 
 use Illuminate\Database\Eloquent\Builder;
-use SuperV\Platform\Domains\Entry\EntryModel;
 use SuperV\Platform\Domains\UI\Table\TableBuilder;
 
 class TaskTableBuilder extends TableBuilder
 {
     protected $buttons = [];
 
-    public function getColumns()
+    public function getColumns(): ?array
     {
         return [
             'id',
             'title',
-            'status' => 'entry.statusLabel()'
+            'entry.statusLabel()',
+            'info',
+            'entry.subtasks().pluck("title")|join("<br>")',
         ];
     }
 
-    public function onQuerying(Builder $query)
+    public function onQuerying(?Builder $query): void
     {
         $query->where('parent_id', null)->orderBy('id', 'DESC');
     }
