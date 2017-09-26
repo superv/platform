15d6fa71676272f7fc5151b805a0f6cb4a793fdc
Ali Selcuk <maselcuk@gmail.com>
2017-09-27 01:11:57 +0300

    * if href is set then return * if route is set along with entry, make the route together

--- src/Domains/UI/Button/Jobs/NormalizeButtonUrl.php
+++ src/Domains/UI/Button/Jobs/NormalizeButtonUrl.php
@@ -2,8 +2,8 @@
 
 namespace SuperV\Platform\Domains\UI\Button\Jobs;
 
-use SuperV\Platform\Domains\UI\Page\Page;
 use SuperV\Platform\Domains\Entry\EntryModel;
+use SuperV\Platform\Domains\UI\Page\Page;
 
 class NormalizeButtonUrl
 {
@@ -23,27 +23,37 @@ class NormalizeButtonUrl
     {
         if ($href = array_pull($this->params, 'href')) {
             array_set($this->params, 'attributes.href', $href);
+
+            return $this->params;
         }
 
-        if ($entry = array_get($this->arguments, 'entry')) {
-            if ($entry instanceof EntryModel) {
-                $verb = array_get($this->params, 'button');
-                // If entry model has a page for this verb, use it,
-                // otherwise check the entry router, for a (default) route
+        $entry = array_get($this->arguments, 'entry');
+        $route = array_pull($this->params, 'route');
 
-                /** @var Page $page */
-                if ($page = $entry->page($verb)) {
-//                    $url = route($page->getRoute(), ['entry' => $entry]);
-                    $url = $page->makeRoute();
-                    array_set($this->params, 'text', $page->getTitle());
+        if ($entry) {
+            if ($entry instanceof EntryModel) {
+
+                if ($route) {
+                    $url = route($route, [$entry::$routeKeyname => $entry]);
                 } else {
-                    $url = $entry->route($verb);
+                    $verb = array_get($this->params, 'button');
+                    // If entry model has a page for this verb, use it,
+                    // otherwise check the entry router, for a (default) route
+
+                    /** @var Page $page */
+                    if ($page = $entry->page($verb)) {
+//                    $url = route($page->getRoute(), ['entry' => $entry]);
+                        $url = $page->makeRoute();
+                        array_set($this->params, 'text', $page->getTitle());
+                    } else {
+                        $url = $entry->route($verb);
+                    }
                 }
                 array_set_if($url, $this->params, 'attributes.href', $url);
             }
         }
 
-        if ($route = array_pull($this->params, 'route')) {
+        if ($route && !$entry) {
             array_set($this->params, 'attributes.href', route($route));
         }
 
