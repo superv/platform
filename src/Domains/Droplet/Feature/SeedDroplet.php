db028e9201c0c83988159ee2ec990920bf9f97cb
Ali Selcuk <maselcuk@gmail.com>
2017-09-28 01:13:22 +0300

    update droplet seeder methpd

--- src/Domains/Droplet/Feature/SeedDroplet.php
+++ src/Domains/Droplet/Feature/SeedDroplet.php
@@ -2,11 +2,7 @@
 
 namespace SuperV\Platform\Domains\Droplet\Feature;
 
-use App\Console\Kernel;
-use SuperV\Platform\Domains\Droplet\Droplet;
 use SuperV\Platform\Domains\Droplet\DropletFactory;
-use SuperV\Platform\Domains\Droplet\Model\DropletCollection;
-use SuperV\Platform\Domains\Droplet\Model\Droplets;
 use SuperV\Platform\Domains\Feature\Feature;
 
 class SeedDroplet extends Feature
@@ -18,14 +14,14 @@ class SeedDroplet extends Feature
         $this->slug = $slug;
     }
 
-    public function handle(Kernel $console, DropletFactory $factory)
+    public function handle(DropletFactory $factory)
     {
         $droplet = $factory->fromSlug($this->slug);
 
         $seeders = $droplet->getSeeders();
 
         foreach ($seeders as $seeder) {
-            $this->dispatch(new $seeder);
+            app($seeder)->seed();
         }
     }
 }
