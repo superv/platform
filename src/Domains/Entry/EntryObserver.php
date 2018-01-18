1140b7afb5159465afc4106481e7a7e55f656412
Ali Selcuk <maselcuk@gmail.com>
2018-01-18 04:29:48 +0300

    clean up

--- src/Domains/Entry/EntryObserver.php
+++ src/Domains/Entry/EntryObserver.php
@@ -3,7 +3,6 @@
 namespace SuperV\Platform\Domains\Entry;
 
 use SuperV\Platform\Contracts\Dispatcher;
-use SuperV\Platform\Domains\Entry\Generic\Features\CreateGenericEntry;
 use SuperV\Platform\Domains\Feature\ServesFeaturesTrait;
 
 class EntryObserver
@@ -26,8 +25,6 @@ class EntryObserver
         if ($callback = $entry->getOnCreateCallback()) {
             return call_user_func($callback, $entry);
         }
-
-//        $this->dispatch(new CreateGenericEntry($entry));
     }
 
     public function updating(EntryModel $entry)
@@ -43,19 +40,7 @@ class EntryObserver
         return true;
     }
 
-    public function saved(EntryModel $entry)
-    {
-//        if ($entry->creating) {
-//            return;
-//        }
-//        $class = get_class($entry->nucleo());
-//
-//        $observer = substr($class, 0, -5) . 'Observer';
-//        $observing = class_exists($observer);
-//        if ($observing) {
-//            (new $observer)->saved($entry);
-//        }
-    }
+    public function saved(EntryModel $entry) { }
 
     public function deleting(EntryModel $entry)
     {
