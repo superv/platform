44ac8ba162598c825a9d8564e2aac040d1d8c6ae
Ali Selcuk <maselcuk@gmail.com>
2018-01-31 11:04:19 +0300

    Adds scope filter to Migrator

--- src/Domains/Database/Migrations/DatabaseMigrationRepository.php
+++ src/Domains/Database/Migrations/DatabaseMigrationRepository.php
@@ -4,6 +4,20 @@ namespace SuperV\Platform\Domains\Database\Migrations;
 
 class DatabaseMigrationRepository extends \Illuminate\Database\Migrations\DatabaseMigrationRepository
 {
+    protected $migration;
+
+    protected $scope;
+
+    public function getMigrations($steps)
+    {
+        return $this->filterScope(parent::getMigrations($steps));
+    }
+
+    public function getLast()
+    {
+        return $this->filterScope(parent::getLast());
+    }
+
     public function createRepository()
     {
         parent::createRepository();
@@ -12,5 +26,60 @@ class DatabaseMigrationRepository extends \Illuminate\Database\Migrations\Databa
         $schema->table($this->table, function ($table) {
             $table->string('scope')->nullable();
         });
+    }
+
+    public function log($file, $batch)
+    {
+        $record = [
+            'migration' => $file,
+            'batch'     => $batch,
+        ];
+
+        if ($this->migration) {
+            if ($this->migration instanceof Migration) {
+                array_set($record, 'scope', $this->migration->scope());
+            }
+        }
+
+        $this->table()->insert($record);
+    }
+
+    /**
+     * @param mixed $migration
+     */
+    public function setMigration($migration)
+    {
+        $this->migration = $migration;
+    }
+
+    /**
+     * @param mixed $scope
+     */
+    public function setScope($scope)
+    {
+        $this->scope = $scope;
+
+        return $this;
+    }
+
+    /**
+     * @param $migrations
+     *
+     * @return array
+     */
+    public function filterScope($migrations)
+    {
+        if (! $this->scope) {
+            return $migrations;
+        }
+
+        $migrations = collect($migrations)->filter(
+            function ($migration) {
+                return $migration->scope === $this->scope;
+            })->values()->toArray();
+
+        $this->scope = null;
+
+        return $migrations;
     }
 }
