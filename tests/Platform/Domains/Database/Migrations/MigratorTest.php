95177f016c0c8278b9d5ed1058b3ac6bcad27148
Ali Selcuk <maselcuk@gmail.com>
2018-01-31 11:04:19 +0300

    Adds scope filter to Migrator

--- tests/Platform/Domains/Database/Migrations/MigratorTest.php
+++ tests/Platform/Domains/Database/Migrations/MigratorTest.php
@@ -3,11 +3,13 @@
 namespace Tests\SuperV\Platform\Domains\Database\Migrations;
 
 use Illuminate\Database\Migrations\Migrator as BaseMigrator;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use SuperV\Platform\Domains\Database\Migrations\Migrator;
 use Tests\SuperV\Platform\BaseTestCase;
 
 class MigratorTest extends BaseTestCase
 {
+    use RefreshDatabase;
     /**
      * @test
      */
@@ -16,5 +18,29 @@ class MigratorTest extends BaseTestCase
         $migrator = new Migrator(app('migration.repository'), app('db'), app('files'));
 
         $this->assertInstanceOf(BaseMigrator::class, $migrator);
+    }
+
+    /**
+     * @test
+     */
+    function save_migrations_scope_if_it_has_one()
+    {
+        $this->app['migrator']->run(__DIR__. '/migrations');
+
+        $this->assertDatabaseHas('migrations', ['scope' => 'foo']);
+        $this->assertDatabaseHas('migrations', ['scope' => 'bar']);
+    }
+
+    /**
+     * @test
+     */
+    function rollback_migrations_by_scope()
+    {
+        $this->app['migrator']->run(__DIR__. '/migrations');
+
+        $this->app['migrator']->setScope('bar')->rollback(__DIR__. '/migrations');
+
+        $this->assertDatabaseMissing('migrations', ['scope' => 'bar']);
+        $this->assertDatabaseHas('migrations', ['scope' => 'foo']);
     }
 }
