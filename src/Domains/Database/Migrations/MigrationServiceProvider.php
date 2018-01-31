774bc00e48c4092aeec16d5217639d16af2d8996
Ali Selcuk <maselcuk@gmail.com>
2018-01-31 09:30:32 +0300

    Hack into framework's Migrations and add column "scope" to migrations table

---
+++ src/Domains/Database/Migrations/MigrationServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SuperV\Platform\Domains\Database\Migrations;
+
+use Illuminate\Database\Migrations\MigrationRepositoryInterface;
+
+class MigrationServiceProvider extends \Illuminate\Database\MigrationServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind(MigrationRepositoryInterface::class, DatabaseMigrationRepository::class);
+        parent::register();
+    }
+
+    protected function registerRepository()
+    {
+        $this->app->singleton('migration.repository', function ($app) {
+            $table = $app['config']['database.migrations'];
+
+            return new DatabaseMigrationRepository($app['db'], $table);
+        });
+    }
+
+    protected function registerMigrator()
+    {
+        $this->app->singleton('migrator', function ($app) {
+            $repository = $app['migration.repository'];
+
+            return new Migrator($repository, $app['db'], $app['files']);
+        });
+    }
+}
---
+++ src/Domains/Database/Migrations/Migrator.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SuperV\Platform\Domains\Database\Migrations;
+
+use Illuminate\Database\Migrations\Migrator as BaseMigrator;
+
+class Migrator extends BaseMigrator
+{
+}
