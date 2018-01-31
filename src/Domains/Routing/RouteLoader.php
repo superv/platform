12cf022a244b7b2cf7f788d6cc28799d316e0100
Ali Selcuk <maselcuk@gmail.com>
2018-01-31 04:01:40 +0300

    Add RouteLoader and tests

---
+++ src/Domains/Routing/RouteLoader.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SuperV\Platform\Domains\Routing;
+
+use Illuminate\Routing\Router;
+
+class RouteLoader
+{
+    /**
+     * @var \Illuminate\Routing\Router
+     */
+    protected $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    public function fromArray(array $routes)
+    {
+        foreach ($routes as $uri => $data) {
+
+            if (str_contains($uri, '@')) {
+                list($verb, $uri) = explode('@', $uri);
+            }
+            $this->router->{$verb ?? 'get'}($uri, $data);
+        }
+    }
+}
