d54c346a776b11ed891f047fa5bd3eb977e3fdcb
Ali Selcuk <maselcuk@gmail.com>
2018-02-02 05:38:40 +0300

    Fixes inheritance bug by pushing registration methods down to base class

--- src/PlatformServiceProvider.php
+++ src/PlatformServiceProvider.php
@@ -2,42 +2,40 @@
 
 namespace SuperV\Platform;
 
-use Illuminate\Foundation\AliasLoader;
-use Illuminate\Support\ServiceProvider;
 use Platform;
-use SuperV\Platform\Listeners\PortDetectedListener;
-use SuperV\Platform\Packs\Database\Migrations\MigrationServiceProvider;
+use SuperV\Platform\Exceptions\CorePlatformException;
 use SuperV\Platform\Facades\PlatformFacade;
+use SuperV\Platform\Listeners\PortDetectedListener;
 use SuperV\Platform\Listeners\RouteMatchedListener;
+use SuperV\Platform\Packs\Database\Migrations\MigrationServiceProvider;
 use SuperV\Platform\Packs\Port\PortDetectedEvent;
+use SuperV\Platform\Providers\BaseServiceProvider;
 use SuperV\Platform\Providers\ThemeServiceProvider;
 
-class PlatformServiceProvider extends ServiceProvider
+class PlatformServiceProvider extends BaseServiceProvider
 {
     protected $providers = [
         MigrationServiceProvider::class,
-        ThemeServiceProvider::class
+        ThemeServiceProvider::class,
     ];
-    protected $bindings = [];
 
     protected $aliases = [
-        'Platform' => PlatformFacade::class
+        'Platform' => PlatformFacade::class,
     ];
-
-    protected $singletons = [];
 
     protected $listeners = [
         'Illuminate\Routing\Events\RouteMatched' => RouteMatchedListener::class,
-        PortDetectedEvent::class => PortDetectedListener::class
+        PortDetectedEvent::class                 => PortDetectedListener::class,
     ];
 
     public function register()
     {
-        $this->registerProviders($this->providers);
-        $this->registerBindings($this->bindings);
-        $this->registerSingletons($this->singletons);
-        $this->registerAliases($this->aliases);
-        $this->registerListeners($this->listeners);
+        if (config('superv.platform.registered') === true) {
+            throw new CorePlatformException('Platform already registered');
+        }
+        parent::register();
+
+        config(['superv.platform.registered' => true]);
     }
 
     public function boot()
@@ -45,59 +43,5 @@ class PlatformServiceProvider extends ServiceProvider
         if (config('superv.installed') === true) {
             Platform::boot();
         }
-    }
-
-    public function registerProviders(array $providers)
-    {
-        collect($providers)
-            ->map(function ($provider) {
-                $this->app->register($provider);
-            });
-    }
-    public function registerBindings(array $bindings)
-    {
-        collect($bindings)
-            ->map(function ($concrete, $abstract) {
-                $this->app->bind($abstract, $concrete);
-            });
-    }
-
-    public function registerSingletons(array $singletons)
-    {
-        collect($singletons)
-            ->map(function ($concrete, $abstract) {
-                if (is_numeric($abstract) && is_string($concrete)) {
-                    $this->app->singleton($concrete, $concrete);
-                } elseif (! preg_match('/[^A-Za-z._\-]/', $abstract)) {
-                    $this->app->singleton("superv.{$abstract}", $concrete);
-                } else {
-                    $this->app->singleton($abstract, $concrete);
-                }
-            });
-    }
-
-    public function registerAliases(array $aliases)
-    {
-        AliasLoader::getInstance($aliases)->register();
-    }
-
-    public function registerListeners(array $listeners)
-    {
-        collect($listeners)
-            ->map(function ($listener, $event) {
-                if (! is_array($listener)) {
-                    $listener = [$listener];
-                }
-                collect($listener)->map(function ($listener) use ($event) {
-                    $this->app['events']->listen($event, $listener);
-                });
-            });
-    }
-
-    public function addViewNamespaces($namespaces)
-    {
-        collect($namespaces)->map(function ($path, $hint) {
-            $this->app['view']->addNamespace($hint, $path);
-        });
     }
 }
