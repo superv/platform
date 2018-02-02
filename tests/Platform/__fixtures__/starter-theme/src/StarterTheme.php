26aa04bd5b7123796256ea5aff172f3b25781117
Ali Selcuk <maselcuk@gmail.com>
2018-02-02 04:51:28 +0300

    Adds sample theme fixture

---
+++ tests/Platform/__fixtures__/starter-theme/src/StarterTheme.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SuperV\Themes\Starter;
+
+use SuperV\Platform\Packs\Droplet\Droplet;
+
+class StarterTheme extends Droplet
+{
+}
---
+++ tests/Platform/__fixtures__/starter-theme/src/StarterThemeServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SuperV\Themes\Starter;
+
+use SuperV\Platform\Packs\Droplet\ServiceProvider;
+
+class StarterThemeServiceProvider extends ServiceProvider
+{
+}
