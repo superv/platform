8fe2ec0dba4b315e8e2cdbb5b064c4c0836b2c00
Ali Selcuk <maselcuk@gmail.com>
2018-02-02 17:50:50 +0300

    Fix parameter issue

--- __workbench__/superv/platform/src/Packs/Droplet/Console/DropletInstallCommand.php
+++ __workbench__/superv/platform/src/Packs/Droplet/Console/DropletInstallCommand.php
@@ -7,7 +7,7 @@ use SuperV\Platform\Packs\Droplet\Installer;
 
 class DropletInstallCommand extends Command
 {
-    protected $signature = 'droplet:install {slug} {--path}';
+    protected $signature = 'droplet:install {slug} {--path=}';
 
     public function handle(Installer $installer)
     {
