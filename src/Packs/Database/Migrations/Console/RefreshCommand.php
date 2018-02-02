6fbc7475f8b58c80275775902360869f725acc5d
Ali Selcuk <maselcuk@gmail.com>
2018-02-02 08:06:16 +0300

    Adds scope parameter to migrate:refresh command

---
+++ src/Packs/Database/Migrations/Console/RefreshCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SuperV\Platform\Packs\Database\Migrations\Console;
+
+use Symfony\Component\Console\Input\InputOption;
+
+class RefreshCommand extends \Illuminate\Database\Console\Migrations\RefreshCommand
+{
+    public function call($command, array $arguments = [])
+    {
+        if ($this->option('scope')) {
+            if (in_array($command, ['migrate', 'migrate:rollback', 'migrate:reset'])) {
+                array_set($arguments, '--scope', $this->option('scope'));
+            }
+        }
+
+        return parent::call($command, $arguments);
+    }
+
+    public function getOptions()
+    {
+        return array_merge(
+            parent::getOptions(),
+            [
+                ['scope', null, InputOption::VALUE_OPTIONAL, 'The scope to rollback for.'],
+            ]
+        );
+    }
+}
