4ac34fda17d6ff39b608ec68df26f46653b38fef
Ali Selcuk <maselcuk@gmail.com>
2018-02-06 12:57:41 +0300

    Moves platform migrations into its own place

---
+++ database/migrations/2018_01_22_001224_creates_fields_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use SuperV\Platform\Packs\Database\Migrations\Migration;
+
+class CreatesFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fields', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('prototype_id');
+            $table->string('slug');
+            $table->string('type');
+            $table->boolean('required')->default(true);
+            $table->string('default_value')->nullable();
+            $table->text('rules')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('fields');
+    }
+}
