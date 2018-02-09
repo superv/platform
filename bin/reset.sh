5acb434380756c11260bd48026b160d26a8cc4bc
Ali Selcuk <maselcuk@gmail.com>
2018-02-09 12:13:28 +0300

    Install these droplets while resetting

--- bin/reset.sh
+++ bin/reset.sh
@@ -5,5 +5,7 @@ echo "superv-mono"| xargs -I {} sh -c "mysql -Nse 'show tables' {}| xargs -I[] m
 sed -i '' 's/^SUPERV_INSTALLED=true/SUPERV_INSTALLED=false/g' .env
 
 php artisan superv:install
+php artisan droplet:install superv.supreme --path=droplets/superv/droplets/supreme
+php artisan droplet:install themes.tailwind --path=droplets/superv/themes/tailwind
 #php artisan superv:install --modules=acp,web,ui,nucleo,supreme
 #php artisan acp:create-user "Ali Selcuk" maselcuk@gmail.com secret
