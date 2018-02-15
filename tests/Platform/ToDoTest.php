1c3fc0c7aa5ce46ad4bf64f49a274451689288f8
Ali Selcuk <maselcuk@gmail.com>
2018-02-15 18:38:30 +0300

    Add TodoTest for task listing purposes

---
+++ tests/Platform/ToDoTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\SuperV\Platform;
+
+/**
+ * Class ToDoTest
+ *
+ * Temporary location for todos
+ *
+ * @package Tests\SuperV\Platform
+ * @ignore
+ */
+class ToDoTest extends BaseTestCase
+{
+    /** @test */
+    function current_global_handler_dhh()
+    {
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    function locator_locates_droplet_from_slug_if_possible()
+    {
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    function platform_detects_active_module_from_route_data()
+    {
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    function platform_add_view_hint_module_for_active_module()
+    {
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    function seeder_seeds_droplet_seeds()
+    {
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    function make_use_of_symfony_finder_while_interacting_with_files()
+    {
+        $this->addToAssertionCount(1);
+    }
+}
