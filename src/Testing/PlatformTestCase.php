9477c91122eeee73f12ab74f1aeb83569db0b65b
Ali Selcuk <maselcuk@gmail.com>
2018-02-17 21:31:41 +0300

    Add test helper macros

--- src/Testing/PlatformTestCase.php
+++ src/Testing/PlatformTestCase.php
@@ -2,9 +2,11 @@
 
 namespace SuperV\Platform\Testing;
 
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestResponse;
+use PHPUnit\Framework\Assert;
 use SuperV\Platform\Domains\Droplet\Installer;
 use SuperV\Platform\PlatformServiceProvider;
 use Tests\CreatesApplication;
@@ -40,12 +42,31 @@ class PlatformTestCase extends TestCase
         }
 
         (new PlatformServiceProvider($this->app))->boot();
+
+        $this->setUpMacros();
     }
 
     protected function setUpMacros()
     {
         TestResponse::macro('data', function ($key) {
             return $this->original->getData()[$key];
+        });
+
+        EloquentCollection::macro('assertContains', function ($value) {
+            Assert::assertTrue($this->contains($value), "Failed asserting that the collection contains the specified value.");
+        });
+
+        EloquentCollection::macro('assertNotContains', function ($value) {
+            Assert::assertFalse($this->contains($value), "Failed asserting that the collection does not contain the specified value.");
+        });
+
+        EloquentCollection::macro('assertEquals', function ($items) {
+            Assert::assertEquals(count($this), count($items));
+
+            $this->zip($items)->each(function ($pair) {
+                list($a, $b) = $pair;
+                Assert::assertTrue($a->is($b));
+            });
         });
     }
 
