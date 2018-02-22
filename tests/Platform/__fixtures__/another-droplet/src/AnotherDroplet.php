605584967b5fca413e1886a045178d2632ab2c81
Ali Selcuk <maselcuk@gmail.com>
2018-02-22 12:47:27 +0300

    Add subdroplets to install

--- tests/Platform/__fixtures__/another-droplet/src/AnotherDroplet.php
+++ tests/Platform/__fixtures__/another-droplet/src/AnotherDroplet.php
@@ -6,4 +6,7 @@ use SuperV\Platform\Domains\Droplet\Droplet;
 
 class AnotherDroplet extends Droplet
 {
+    protected $installs = [
+        'themes.another_sub' => 'droplets/themes/another_sub-droplet',
+    ];
 }
