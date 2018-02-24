60f87b7ecdbaa133ae2861350baa584359e89ce7
Ali Selcuk <maselcuk@gmail.com>
2018-02-24 17:46:45 +0300

    Add auth test for authenticating with jwt tokens

--- src/Domains/Auth/User.php
+++ src/Domains/Auth/User.php
@@ -6,8 +6,9 @@ use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 use SuperV\Platform\Domains\Auth\Contracts\User as UserContract;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model implements UserContract, AuthenticatableContract
+class User extends Model implements UserContract, AuthenticatableContract, JWTSubject
 {
     use Authenticatable;
 
@@ -16,7 +17,7 @@ class User extends Model implements UserContract, AuthenticatableContract
     protected $guarded = [];
 
     protected $casts = [
-        'ports' => 'json'
+        'ports' => 'json',
     ];
 
     public function profile()
@@ -27,5 +28,25 @@ class User extends Model implements UserContract, AuthenticatableContract
     public function createProfile(array $attributes)
     {
         return $this->profile()->create($attributes);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
