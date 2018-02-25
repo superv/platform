511493d618cce8b61b7bce588609e51d80372ab6
Ali Selcuk <maselcuk@gmail.com>
2018-02-25 09:05:35 +0300

    Check for allowed user types if port has a filter

--- src/Domains/Auth/PlatformUserProvider.php
+++ src/Domains/Auth/PlatformUserProvider.php
@@ -70,8 +70,10 @@ class PlatformUserProvider extends EloquentUserProvider
             }
         }
 
-//        if ($port = \Platform::port()) {
-//            $query->whereIn('type', $port->allowedUserTypes());
-//        }
+        if ($port = \Platform::port()) {
+            if ($allowedUserTypes = $port->allowedUserTypes()) {
+                $query->whereIn('type', $allowedUserTypes);
+            }
+        }
     }
 }
