28119f809f70e26ba91f0a3dfc8bbfd9abc35a80
Ali Selcuk <maselcuk@gmail.com>
2018-06-28 23:25:50 +0300

    Add experimental user table builder

---
+++ src/Domains/Auth/UserTableBuilder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SuperV\Platform\Domains\Auth;
+
+use SuperV\Platform\Domains\Table\TableBuilder;
+
+class UserTableBuilder extends TableBuilder
+{
+    protected $model = User::class;
+
+    protected $columns = [
+        'id'    => [
+            'attr'    => 'id',
+            'heading' => 'ID',
+        ],
+        'email' => [
+            'attr'    => 'email',
+            'heading' => 'Email',
+        ],
+    ];
+}
