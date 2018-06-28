1afe6a07f1a0a2cb88af83d04e7e965f6f277bee
Ali Selcuk <maselcuk@gmail.com>
2018-06-28 21:29:43 +0300

    Add support for Entry Observing

--- src/Domains/Entry/EntryModel.php
+++ src/Domains/Entry/EntryModel.php
@@ -2,11 +2,17 @@
 
 namespace SuperV\Platform\Domains\Entry;
 
+use Closure;
 use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class EntryModel extends Model
 {
     protected $guarded = [];
+
+    protected $onCreate;
+
+    protected $hasUUID;
 
     public static $rules;
 
@@ -24,6 +30,66 @@ class EntryModel extends Model
         }
 
         return static::$rules;
+    }
+
+    protected static function boot()
+    {
+        $instance = new static;
+
+        $class = get_class($instance);
+        $events = $instance->getObservableEvents();
+        $observer = str_replace_last('Model', '', $class).'Observer';
+        $observing = class_exists($observer);
+
+        if ($events && $observing) {
+            self::observe(app($observer));
+        }
+
+        if ($events && ! $observing) {
+            self::observe(EntryObserver::class);
+        }
+
+        if ($instance->hasUUID) {
+            $instance->incrementing = false;
+            static::creating(function (Model $model) {
+                if (! isset($model->attributes[$model->getKeyName()])) {
+                    $model->incrementing = false;
+                    $uuid = Uuid::uuid4();
+                    $model->attributes[$model->getKeyName()] = str_replace('-', '', $uuid->toString());
+                }
+            });
+        }
+
+        parent::boot();
+    }
+
+    public function getId()
+    {
+        return $this->getKey();
+    }
+
+    public function flushCache()
+    {
+    }
+
+    /**
+     * @param Closure $callback
+     *
+     * @return EntryModel
+     */
+    public function onCreate(Closure $callback)
+    {
+        $this->onCreate = $callback;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOnCreateCallback()
+    {
+        return $this->onCreate;
     }
 
 }
