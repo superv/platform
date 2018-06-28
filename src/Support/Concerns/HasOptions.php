331a4aa1c019c08ce9724453e163257ee96e21d1
Ali Selcuk <maselcuk@gmail.com>
2018-06-28 21:31:54 +0300

    Add support concerns

---
+++ src/Support/Concerns/HasOptions.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SuperV\Platform\Support\Concerns;
+
+use Illuminate\Support\Collection;
+
+trait HasOptions
+{
+    /** @var Collection */
+    protected $options;
+
+    public function setOptions(Collection $options)
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function setOption($key, $value)
+    {
+        $this->options->put($key, $value);
+
+        return $this;
+    }
+
+    /**
+     * @param        $key
+     * @param  null  $default
+     * @return mixed
+     */
+    public function getOption($key, $default = null)
+    {
+        return $this->options->get($key, $default);
+    }
+}
---
+++ src/Support/Concerns/Parental/HasParentModel.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace SuperV\Platform\Support\Concerns\Parental;
+
+use Illuminate\Support\Str;
+use ReflectionClass;
+
+trait HasParentModel
+{
+    public $hasParentModel = true;
+
+    public static function bootHasParentModel()
+    {
+        static::creating(function ($model) {
+            if ($model->parentHasReturnsChildModelsTrait()) {
+                $model->forceFill(
+                    [$model->getInhertanceColumn() => $model->classToAlias(get_class($model))]
+                );
+            }
+        });
+
+        static::addGlobalScope(function ($query) {
+            $instance = new static;
+
+            if ($instance->parentHasReturnsChildModelsTrait()) {
+                $query->where($instance->getInhertanceColumn(), $instance->classToAlias(get_class($instance)));
+            }
+        });
+    }
+
+    public function parentHasReturnsChildModelsTrait()
+    {
+        return $this->returnsChildModels ?? false;
+    }
+
+    public function getTable()
+    {
+        if (! isset($this->table)) {
+            return str_replace('\\', '', Str::snake(Str::plural(class_basename($this->getParentClass()))));
+        }
+
+        return $this->table;
+    }
+
+    public function getForeignKey()
+    {
+        return Str::snake(class_basename($this->getParentClass())).'_'.$this->primaryKey;
+    }
+
+    public function joiningTable($related)
+    {
+        $relatedClassName = method_exists((new $related), 'getClassNameForRelationships')
+            ? (new $related)->getClassNameForRelationships()
+            : class_basename($related);
+
+        $models = [
+            Str::snake($relatedClassName),
+            Str::snake($this->getClassNameForRelationships()),
+        ];
+
+        sort($models);
+
+        return strtolower(implode('_', $models));
+    }
+
+    public function getClassNameForRelationships()
+    {
+        return class_basename($this->getParentClass());
+    }
+
+    protected function getParentClass()
+    {
+        static $parentClassName;
+
+        return $parentClassName ?: $parentClassName = (new ReflectionClass($this))->getParentClass()->getName();
+    }
+}
