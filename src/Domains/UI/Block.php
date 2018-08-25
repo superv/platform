d4b54eb47edc4af3f1956e8b356236e18620eb90
Ali Selcuk <maselcuk@gmail.com>
2018-08-26 00:06:06 +0300

    Implement JsonSerializable for auto serialize

--- src/Domains/UI/Block.php
+++ src/Domains/UI/Block.php
@@ -2,10 +2,7 @@
 
 namespace SuperV\Platform\Domains\UI;
 
-
-use Illuminate\Contracts\Support\Arrayable;
-
-class Block implements Arrayable
+class Block implements \JsonSerializable
 {
     protected $component;
 
@@ -33,5 +30,18 @@ class Block implements Arrayable
             'component' => $this->component,
             'props'     => $this->props,
         ];
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     *
+     * @link  https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
     }
 }
