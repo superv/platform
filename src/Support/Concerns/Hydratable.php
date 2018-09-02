91bf347eb01dc076dd40e285ba5acc2d51fa8986
Ali Selcuk <maselcuk@gmail.com>
2018-09-03 00:53:17 +0300

    Use only hydratable parameters if given

--- src/Support/Concerns/Hydratable.php
+++ src/Support/Concerns/Hydratable.php
@@ -6,6 +6,14 @@ trait Hydratable
 {
     public function hydrate(array $parameters)
     {
+        if (empty($parameters)) {
+            return $this;
+        }
+
+        if (isset($this->hydratables)) {
+            $parameters = array_intersect_key($parameters, array_flip($this->hydratables));
+        }
+
         foreach ($parameters as $parameter => $value) {
             $method = camel_case('set_'.$parameter);
 
