97cf3f89c15a48cc011ba391461494d33db827e0
Ali Selcuk <maselcuk@gmail.com>
2018-09-04 13:51:54 +0300

    Clean up

--- src/Domains/Auth/User.php
+++ src/Domains/Auth/User.php
@@ -4,7 +4,6 @@ namespace SuperV\Platform\Domains\Auth;
 
 use App\Notifications\ResetPassword;
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\Model;
