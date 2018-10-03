a4d43d3bc45957fd1912d344aae68691c055887a
Ali Selcuk <maselcuk@gmail.com>
2018-10-03 10:18:21 +0300

    Remove dev block

--- src/Http/Controllers/BaseApiController.php
+++ src/Http/Controllers/BaseApiController.php
@@ -2,18 +2,11 @@
 
 namespace SuperV\Platform\Http\Controllers;
 
-use SuperV\Platform\Domains\Auth\Users;
-
 class BaseApiController extends BaseController
 {
     public function __construct()
     {
         parent::__construct();
-
-        if (app()->environment() === 'local' && ! $this->request->hasHeader('authorization')) {
-            $user = $this->request->get('user', 'root');
-            auth()->onceUsingId(Users::withEmail("{$user}@superv.io")->id);
-        }
 
         $this->middleware('auth:superv-api');
     }
