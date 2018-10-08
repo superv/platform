4c50c89c37308d3ecd1612402993fffd8fd85247
Ali Selcuk <maselcuk@gmail.com>
2018-10-08 17:26:50 +0300

    Injecting guard causes problems

--- src/Support/Current.php
+++ src/Support/Current.php
@@ -2,7 +2,6 @@
 
 namespace SuperV\Platform\Support;
 
-use Illuminate\Contracts\Auth\Guard;
 use SuperV\Platform\Domains\Auth\Contracts\Users;
 
 class Current
@@ -12,40 +11,64 @@ class Current
      */
     protected $users;
 
-    /**
-     * @var \Illuminate\Contracts\Auth\Guard
-     */
-    protected $guard;
-
     protected $user;
 
-    public function __construct(Users $users, Guard $guard)
+    public function __construct(Users $users)
     {
         $this->users = $users;
-        $this->guard = $guard;
     }
 
     /**
-     * Returns the current logged in user
+     * Return the current logged in user
      *
      * @return \SuperV\Platform\Domains\Auth\Contracts\User
      */
     public function user()
     {
         if (! $this->user) {
-            $this->user = $this->users->find($this->guard->id());
+            $this->user = $this->users->find(auth()->guard()->id());
         }
 
         return $this->user;
     }
 
     /**
-     * Returns the current platform port
+     * Return the current platform port
      *
      * @return \SuperV\Platform\Domains\Port\Port
      */
     public function port()
     {
         return \Platform::port();
+    }
+
+    /**
+     * Return current application environment
+     *
+     * @return string
+     */
+    public function env()
+    {
+        return app()->environment();
+    }
+
+    /**
+     * Return true if we are on local
+     *
+     * @return bool
+     */
+    public function envIsLocal()
+    {
+        return $this->env() === 'local';
+    }
+
+    /**
+     * Return true if we are on production
+     *
+     * @return bool
+     */
+    public function envIsProduction()
+    {
+        return $this->env() === 'production';
     }
 }
