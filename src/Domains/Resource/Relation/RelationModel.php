20cca57457e7b56d495da1556766174c8e54159f
Ali Selcuk <maselcuk@gmail.com>
2018-11-05 21:21:42 +0300

    Add support for getting relations from cache

--- src/Domains/Resource/Relation/RelationModel.php
+++ src/Domains/Resource/Relation/RelationModel.php
@@ -5,18 +5,40 @@ namespace SuperV\Platform\Domains\Resource\Relation;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use SuperV\Platform\Domains\Entry\EntryModelV2;
+use SuperV\Platform\Domains\Resource\ResourceModel;
 
 class RelationModel extends EntryModelV2
 {
     protected $table = 'sv_relations';
 
     protected $casts = [
-        'config' => 'array'
+        'config' => 'array',
     ];
+
+    public function resource()
+    {
+        return $this->belongsTo(ResourceModel::class, 'resource_id');
+    }
 
     public function getType()
     {
         return $this->type;
+    }
+
+    public static function fromCache($resourceHandle, $relationName)
+    {
+        $cacheKey = 'sv:relations:'.$resourceHandle.':'.$relationName;
+
+        $entry = cache()->rememberForever($cacheKey, function () use ($resourceHandle, $relationName) {
+            return static::query()
+                         ->whereHas('resource', function ($query) use ($resourceHandle) {
+                             $query->where('slug', $resourceHandle);
+                         })
+                         ->where('name', $relationName)
+                         ->first();
+        });
+
+        return $entry;
     }
 
     protected static function boot()
