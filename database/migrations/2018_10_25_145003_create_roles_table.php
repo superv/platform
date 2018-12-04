5abe85a65b86106995e789ea85f2a289a687b6c9
Ali Selcuk <maselcuk@gmail.com>
2018-12-04 09:08:24 +0300

    Merge migrations, add resource labels

--- database/migrations/2018_10_25_145003_create_roles_table.php
+++ database/migrations/2018_10_25_145003_create_roles_table.php
@@ -1,21 +1,23 @@
 <?php
 
+use SuperV\Platform\Domains\Database\Migrations\Migration;
 use SuperV\Platform\Domains\Database\Schema\Blueprint;
 use SuperV\Platform\Domains\Database\Schema\Schema;
-use SuperV\Platform\Domains\Database\Migrations\Migration;
 
 class CreateRolesTable extends Migration
 {
     public function up()
     {
         Schema::create('auth_roles', function (Blueprint $table) {
+            $table->resourceBlueprint()->label('Roles');
+
             $table->increments('id');
             $table->string('slug')->unique();
             $table->timestamps();
         });
 
-
         Schema::create('auth_assigned_roles', function (Blueprint $table) {
+            $table->resourceBlueprint()->label('Assigned Roles');
             $table->increments('id');
             $table->morphs('owner');
             $table->unsignedInteger('role_id');
@@ -23,7 +25,16 @@ class CreateRolesTable extends Migration
             $table->timestamps();
         });
 
+        Schema::create('auth_actions', function (Blueprint $table) {
+            $table->resourceBlueprint()->label('Actions');
+
+            $table->increments('id');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+
         Schema::create('auth_assigned_actions', function (Blueprint $table) {
+            $table->resourceBlueprint()->label('Assigned Actions');
             $table->increments('id');
             $table->morphs('owner');
             $table->unsignedInteger('action_id');
