0987b70ee7c5fdac66d267e0ac5fb465988da34b
Ali Selcuk <maselcuk@gmail.com>
2018-12-05 23:40:20 +0300

    Make dictionary field work

--- src/Domains/Resource/Field/Types/DictionaryField.php
+++ src/Domains/Resource/Field/Types/DictionaryField.php
@@ -2,9 +2,39 @@
 
 namespace SuperV\Platform\Domains\Resource\Field\Types;
 
+use SuperV\Platform\Domains\Database\Model\Contracts\EntryContract;
 use SuperV\Platform\Domains\Resource\Contracts\NeedsDatabaseColumn;
 
 class DictionaryField extends FieldType implements NeedsDatabaseColumn
 {
-    protected $type = 'textarea';
+    protected function boot()
+    {
+        $this->on('form.accessing', $this->accessor());
+        $this->on('form.mutating', $this->mutator());
+        $this->on('view.presenting', $this->accessor());
+        $this->on('table.presenting', $this->accessor());
+    }
+
+    protected function accessor()
+    {
+        return function ($value) {
+            if (is_string($value)) {
+                return json_decode($value, true);
+            }
+
+            return $value;
+        };
+    }
+
+    protected function mutator()
+    {
+        return function ($value, EntryContract $entry) {
+//            dump($value);
+//            if (!is_string($value)) {
+//                return $entry->setAttribute($this->getColumnName(),json_encode($value));
+//            }
+
+            return $value;
+        };
+    }
 }
