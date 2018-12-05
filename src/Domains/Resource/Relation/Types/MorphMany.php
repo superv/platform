632f2842d94cc8128701f230442f619030d7ce8e
Ali Selcuk <maselcuk@gmail.com>
2018-12-05 15:06:11 +0300

    Add form and table action for morphmany

--- src/Domains/Resource/Relation/Types/MorphMany.php
+++ src/Domains/Resource/Relation/Types/MorphMany.php
@@ -5,11 +5,15 @@ namespace SuperV\Platform\Domains\Resource\Relation\Types;
 use Illuminate\Database\Eloquent\Relations\MorphMany as EloquentMorphMany;
 use Illuminate\Database\Eloquent\Relations\Relation as EloquentRelation;
 use SuperV\Platform\Domains\Database\Model\Contracts\EntryContract;
+use SuperV\Platform\Domains\Resource\Action\ModalAction;
+use SuperV\Platform\Domains\Resource\Contracts\ProvidesForm;
 use SuperV\Platform\Domains\Resource\Contracts\ProvidesTable;
+use SuperV\Platform\Domains\Resource\Form\Form;
+use SuperV\Platform\Domains\Resource\Form\FormConfig;
 use SuperV\Platform\Domains\Resource\Relation\Relation;
 use SuperV\Platform\Domains\Resource\Table\ResourceTable;
 
-class MorphMany extends Relation implements ProvidesTable
+class MorphMany extends Relation implements ProvidesTable, ProvidesForm
 {
     protected function newRelationQuery(EntryContract $relatedEntryInstance): EloquentRelation
     {
@@ -28,6 +32,21 @@ class MorphMany extends Relation implements ProvidesTable
             ->setResource($this->getRelatedResource())
             ->setQuery($this)
             ->setDataUrl(url()->current().'/data')
-            ->mergeFields($this->getPivotFields());
+            ->addContextAction(
+                ModalAction::make('New '.str_singular(str_unslug($this->getName())))
+                           ->setModalUrl($this->route('create', $this->parentEntry))
+            );
+//            ->mergeFields($this->getPivotFields());
+    }
+
+    public function makeForm(): Form
+    {
+        return FormConfig::make($this->newQuery()->make())
+                         ->hideField(sv_resource($this->parentEntry)->getResourceKey().'_id')
+                         ->makeForm();
+    }
+
+    public function getFormTitle(): string
+    {
     }
 }
