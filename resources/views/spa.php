9a8c9d45d7e894ce330d34f79cc213e2c3adfb0b
Ali Selcuk <maselcuk@gmail.com>
2019-02-28 04:23:47 +0300

    Update SPA

--- resources/views/spa.php
+++ resources/views/spa.php
@@ -1,28 +1,5 @@
-<!DOCTYPE html>
-<html lang=en>
-<head>
-  <meta charset=utf-8>
-  <meta content="IE=edge" http-equiv=X-UA-Compatible>
-  <meta content="width=device-width,initial-scale=1" name=viewport>
-  <link href=https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css rel=stylesheet>
-  <link crossorigin=anonymous href=https://use.fontawesome.com/releases/v5.3.1/css/all.css
-        integrity=sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU rel=stylesheet>
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel=stylesheet>
-  <style>.font-sans {
+<!DOCTYPE html><html lang=en><head><meta charset=utf-8><meta content="IE=edge" http-equiv=X-UA-Compatible><meta content="width=device-width,initial-scale=1" name=viewport><link href=https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css rel=stylesheet><link crossorigin=anonymous href=https://use.fontawesome.com/releases/v5.3.1/css/all.css integrity=sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU rel=stylesheet><link href="https://fonts.googleapis.com/css?family=Montserrat" rel=stylesheet><style>.font-sans {
       font-family: Montserrat, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
       -webkit-font-smoothing: antialiased;
       -moz-osx-font-smoothing: grayscale;
-    }</style>
-  <title>SuperV Project</title>
-  <link href=/vendor/superv/js/app.01a0c558.js rel=preload as=script>
-  <link href=/vendor/superv/js/chunk-vendors.18f7e491.js rel=preload as=script>
-</head>
-<body class="font-sans font-normal antialiased text-smooth min-h-screen">
-  <noscript><strong>We're sorry but frontend doesn't work at all without JavaScript enabled. Please enable it to
-      continue.</strong></noscript>
-  <div id=app></div>
-  <script id=config type=application/json><?php echo json_encode($config); ?></script>
-  <script src=/vendor/superv/js/chunk-vendors.18f7e491.js></script>
-  <script src=/vendor/superv/js/app.01a0c558.js></script>
-</body>
-</html>
+    }</style><title>SuperV Project</title><link href=/vendor/superv/js/app.ae138c00.js rel=preload as=script><link href=/vendor/superv/js/chunk-vendors.dcf3a24b.js rel=preload as=script></head><body class="font-sans font-normal antialiased text-smooth min-h-screen"><noscript><strong>We're sorry but frontend doesn't work at all without JavaScript enabled. Please enable it to continue.</strong></noscript><div id=app></div><script id=config type=application/json><?php echo json_encode($config); ?></script><script src=/superv/js/chunk-vendors.dcf3a24b.js></script><script src=/superv/js/app.ae138c00.js></script></body></html>
