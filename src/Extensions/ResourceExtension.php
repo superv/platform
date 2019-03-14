7c68a5c3cd98f76833b78760d8412b31431547a5
Ali Selcuk <maselcuk@gmail.com>
2019-03-14 23:21:48 +0300

    Add delete button

--- src/Extensions/ResourceExtension.php
+++ src/Extensions/ResourceExtension.php
@@ -2,8 +2,10 @@
 
 namespace SuperV\Platform\Extensions;
 
+use SuperV\Platform\Domains\Resource\Action\DeleteEntryAction;
 use SuperV\Platform\Domains\Resource\Extension\Contracts\ExtendsResource;
 use SuperV\Platform\Domains\Resource\Resource;
+use SuperV\Platform\Domains\Resource\Table\ResourceTable;
 
 class ResourceExtension implements ExtendsResource
 {
@@ -12,6 +14,10 @@ class ResourceExtension implements ExtendsResource
         $resource->searchable(['handle']);
         $fields = $resource->indexFields();
         $fields->get('addon')->copyToFilters();
+
+        $resource->on('index.config', function (ResourceTable $table) {
+            $table->addRowAction(DeleteEntryAction::class);
+        });
     }
 
     public function extends(): string
--- tests/Platform/Domains/Routing/RouteRegistrarTest.php
+++ tests/Platform/Domains/Routing/RouteRegistrarTest.php
@@ -37,6 +37,30 @@ class RouteRegistrarTest extends TestCase
         $this->assertInstanceOf(\Closure::class, $patchRoutes['web/bar']->getAction('uses'));
     }
 
+    function test__registers_hostname_with_different_port()
+    {
+        Hub::register(new class extends Port
+        {
+            protected $slug = 'local';
+
+            protected $hostname = '127.0.0.1:8000';
+        });
+
+        $registrar = $this->app->make(RouteRegistrar::class);
+        $registrar->setPort($this->getPort('local'))->register(['data' => 'WebController@foo']);
+
+
+        $getRoutes = $this->router()->getRoutes()->get('GET');
+
+            $route = $getRoutes['127.0.0.1:8000data'];
+        $this->assertNotNull($route);
+        $this->assertEquals('local', $route->getAction('port'));
+
+
+//        dd($getRoutes['127.0.0.1:8000data']);
+
+    }
+
     /** @test */
     function registers_routes_for_a_port()
     {
