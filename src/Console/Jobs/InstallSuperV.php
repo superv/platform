2077d99f0cda9dfc6c53b2521f2b518b123e025b
Ali Selcuk <maselcuk@gmail.com>
2019-03-25 23:27:35 +0300

    Do not publish assets

--- src/Console/Jobs/InstallSuperV.php
+++ src/Console/Jobs/InstallSuperV.php
@@ -116,8 +116,6 @@ class InstallSuperV
         DB::commit();
 
         Artisan::call('vendor:publish', ['--tag' => 'superv.config']);
-//        Artisan::call('vendor:publish', ['--tag' => 'superv.views']);
-        Artisan::call('vendor:publish', ['--tag' => 'superv.assets']);
 
         PlatformInstalledEvent::dispatch();
 
