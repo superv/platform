ef1aba6714f5462a4fd246ecc1bc4509b18542bc
Ali Selcuk <maselcuk@gmail.com>
2019-03-25 22:58:40 +0300

    Extract spa files to drop addon

--- resources/spa/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-const config = require('/Users/dali/Projects/SuperV/Repos/superv-js/tailwind.config.js')
-module.exports = {
-  theme: config.theme,
-  variants: config.variants,
-  plugins: config.plugins,
-}
