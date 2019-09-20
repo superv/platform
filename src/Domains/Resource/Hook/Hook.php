f54eee26ff7a130137cee004ee940610335deefa
Ali Selcuk <maselcuk@gmail.com>
2019-09-20 21:11:29 +0300

    Use the new Identifier class

--- src/Domains/Resource/Hook/Hook.php
+++ src/Domains/Resource/Hook/Hook.php
@@ -81,33 +81,40 @@ class Hook
         return $this;
     }
 
-    public function hookType($identifier, $type, $hookHandler, $subKey = null)
-    {
-        if (! class_exists($hookHandler)) {
-            return Log::error("Hook handler class does not exist: ".$hookHandler);
-        }
-        $typeHook = str_replace_last("\\Hook", "\\".studly_case($type.'_hook'), get_class($this));
-        if (class_exists($typeHook)) {
-            app($typeHook)->hook($identifier, $hookHandler, $subKey);
-        }
-    }
-
     public function register($identifier, $hookHandler, $className)
     {
-        $subKey = null;
-        $parts = explode('.', $identifier);
-        if (count($parts) > 2) {
-            $identifier = sprintf('%s.%s', $parts[0], $parts[1]);
+        $_identifier = sv_identifier($identifier);
 
-            if (str_contains($parts[2], ':')) {
-                list($hookType, $subKey) = explode(':', $parts[2]);
-            } else {
-                $hookType = $parts[2];
-            }
+        $subKey = null;
+
+//        $parts = explode('.', $identifier);
+//        if (count($parts) > 2) {
+//            $identifier = sprintf('%s.%s', $parts[0], $parts[1]);
+//
+//            if (str_contains($parts[2], ':')) {
+//                list($hookType, $subKey) = explode(':', $parts[2]);
+//            } else {
+//                $hookType = $parts[2];
+//            }
+//        } else {
+//            $parts = explode('_', snake_case($className));
+//            $hookType = end($parts);
+//        }
+
+        if ($_identifier->getNodeCount() > 2) {
+            $hookType = (string)$_identifier->getType();
+            $subKey = $_identifier->getTypeId();
+            $identifier = $_identifier->getParent();
         } else {
             $parts = explode('_', snake_case($className));
             $hookType = end($parts);
+            $subKey2 = null;
         }
+
+//        if ($subKey !== $subKey2 || $hookType !== $hookType2) {
+//            dd($identifier, $subKey, $subKey2);
+//        }
+//
 
         if (! isset($this->map[$identifier])) {
             $this->map[$identifier] = [];
@@ -126,6 +133,17 @@ class Hook
         $this->hookType($identifier, $hookType, $hookHandler, $subKey);
 
         return $this;
+    }
+
+    public function hookType($identifier, $type, $hookHandler, $subKey = null)
+    {
+        if (! class_exists($hookHandler)) {
+            return Log::error("Hook handler class does not exist: ".$hookHandler);
+        }
+        $typeHook = str_replace_last("\\Hook", "\\".studly_case($type.'_hook'), get_class($this));
+        if (class_exists($typeHook)) {
+            app($typeHook)->hook($identifier, $hookHandler, $subKey);
+        }
     }
 
     /** @return static */
