1dc495e05ae41e410f105ef331bd18612daebebe
Ali Selcuk <maselcuk@gmail.com>
2019-10-06 03:57:11 +0300

    Add `store` and `edit` method for verbosity

--- src/Domains/Resource/Router.php
+++ src/Domains/Resource/Router.php
@@ -23,6 +23,16 @@ class Router
         ]);
     }
 
+    public function store()
+    {
+        return $this->createForm();
+    }
+
+    public function edit(EntryContract $entry)
+    {
+        return $this->updateForm($entry);
+    }
+
     public function updateForm(EntryContract $entry)
     {
         return sv_route('sv::forms.display', [
