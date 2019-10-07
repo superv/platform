b6ade1d50ecedb15457f59c59a8f69ede31bf0d6
Ali Selcuk <maselcuk@gmail.com>
2019-10-08 01:38:12 +0300

    Set value to null if request file is invalid

--- src/Domains/Resource/Field/Types/FileField.php
+++ src/Domains/Resource/Field/Types/FileField.php
@@ -4,6 +4,7 @@ namespace SuperV\Platform\Domains\Resource\Field\Types;
 
 use Closure;
 use Illuminate\Database\Query\JoinClause;
+use SplFileInfo;
 use SuperV\Platform\Domains\Database\Model\Contracts\EntryContract;
 use SuperV\Platform\Domains\Media\Media;
 use SuperV\Platform\Domains\Media\MediaBag;
@@ -11,6 +12,7 @@ use SuperV\Platform\Domains\Media\MediaOptions;
 use SuperV\Platform\Domains\Resource\Field\Contracts\HasModifier;
 use SuperV\Platform\Domains\Resource\Field\Contracts\SortsQuery;
 use SuperV\Platform\Domains\Resource\Field\DoesNotInteractWithTable;
+use SuperV\Platform\Domains\Resource\Field\FieldRules;
 use SuperV\Platform\Domains\Resource\Field\FieldType;
 use SuperV\Platform\Support\Composer\Payload;
 
@@ -24,28 +26,30 @@ class FileField extends FieldType implements
     protected function boot()
     {
         $this->field->on('form.composing', $this->composer());
-//        $this->>field->on('form.mutating', $this->mutator());
 
         $this->field->on('view.composing', $this->composer());
         $this->field->on('table.composing', $this->composer());
     }
 
-    public function getMedia(EntryContract $entry, $label): ?Media
+    public function updateRules(FieldRules $rules)
     {
-        $bag = new MediaBag($entry, $label);
+        if (is_null($this->requestFile)) {
+            $rules->removeAll();
+        }
 
-        $query = $bag->media()->where('label', $label)->latest();
-
-        return $query->first();
     }
 
     public function getModifier(): Closure
     {
         return function ($value, ?EntryContract $entry) {
-            $this->requestFile = $value;
+            $this->requestFile = $value instanceof SplFileInfo ? $value : null;
 
             return function () use ($entry) {
                 if (! $this->requestFile || ! $entry) {
+                    return null;
+                }
+
+                if (! $this->requestFile instanceof SplFileInfo) {
                     return null;
                 }
 
@@ -56,6 +60,32 @@ class FileField extends FieldType implements
                 return $media;
             };
         };
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param                                       $direction
+     * @throws \Exception
+     */
+    public function sortQuery($query, $direction)
+    {
+        $model = $query->getModel();
+
+        $query->getQuery()->leftJoin('sv_media', function (JoinClause $join) use ($model) {
+            $join->on('sv_media.id', '=', $model->getQualifiedKeyName());
+            $join->where('sv_media.owner_type', '=', $this->field->identifier()->parent());
+        });
+
+        $query->orderBy('sv_media.size', $direction);
+    }
+
+    public static function getMedia(EntryContract $entry, $label): ?Media
+    {
+        $bag = new MediaBag($entry, $label);
+
+        $query = $bag->media()->where('label', $label)->latest();
+
+        return $query->first();
     }
 
     protected function composer()
@@ -77,22 +107,5 @@ class FileField extends FieldType implements
                            ->disk($this->getConfigValue('disk', 'local'))
                            ->path($this->getConfigValue('path'))
                            ->visibility($this->getConfigValue('visibility', 'private'));
-    }
-
-    /**
-     * @param \Illuminate\Database\Eloquent\Builder $query
-     * @param                                       $direction
-     * @throws \Exception
-     */
-    public function sortQuery($query, $direction)
-    {
-        $model = $query->getModel();
-
-        $query->getQuery()->leftJoin('sv_media', function (JoinClause $join) use ($model) {
-            $join->on('sv_media.id', '=', $model->getQualifiedKeyName());
-            $join->where('sv_media.owner_type', '=', $this->field->identifier()->parent());
-        });
-
-        $query->orderBy('sv_media.size', $direction);
     }
 }
