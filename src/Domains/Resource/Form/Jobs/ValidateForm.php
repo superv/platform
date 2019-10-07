5cbbe3050e3d5adc70fc67dd338478f37c23981b
Ali Selcuk <maselcuk@gmail.com>
2019-10-08 01:43:34 +0300

    Clean up

--- src/Domains/Resource/Form/Jobs/ValidateForm.php
+++ src/Domains/Resource/Form/Jobs/ValidateForm.php
@@ -42,15 +42,9 @@ class ValidateForm
 
     public function handle(Validator $validator)
     {
-        $this->fields->validating($this->data, $this->entry);
-
-//        $rules = (new GetRules($this->fields->visible()))->get($this->entry);
-
         $rules = $this->fields->rules($this->entry);
 
         $data = $this->data->getForValidation($this->entry);
-
-//        dd($rules, $data);
 
         $attributes = $this->fields
             ->map(function (FieldInterface $field) {
