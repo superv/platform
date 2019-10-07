f6e495622ed5122b50c0e56006c0437a2ea4dcff
Ali Selcuk <maselcuk@gmail.com>
2019-10-08 01:42:07 +0300

    Add SubmitForm class

---
+++ src/Domains/Resource/Form/Features/SubmitForm.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SuperV\Platform\Domains\Resource\Form\Features;
+
+use Illuminate\Http\Request;
+use SuperV\Platform\Domains\Database\Model\Contracts\EntryContract;
+use SuperV\Platform\Domains\Resource\Database\Entry\EntryRepository;
+use SuperV\Platform\Domains\Resource\Form\Contracts\FormInterface;
+use SuperV\Platform\Domains\Resource\Form\FormFactory;
+use SuperV\Platform\Domains\Resource\ResourceFactory;
+
+class SubmitForm
+{
+    /** @var \SuperV\Platform\Domains\Resource\Form\Features\Resource */
+    protected $resource;
+
+    /** @var array */
+    protected $formData;
+
+    /** @var \SuperV\Platform\Domains\Database\Model\Contracts\EntryContract */
+    protected $entry;
+
+    public function setResource($resource): SubmitForm
+    {
+        $this->resource = is_string($resource) ? ResourceFactory::make($resource) : $resource;
+
+        return $this;
+    }
+
+    public function setFormData($formData): SubmitForm
+    {
+        $this->formData = $formData;
+
+        return $this;
+    }
+
+    public function submit(): FormInterface
+    {
+        $builder = FormFactory::builderFromResource($this->resource);
+        $builder->setEntry($this->entry ?? EntryRepository::for($this->resource)->newInstance());
+
+        $builder->setRequest(new Request($this->formData));
+
+        $form = $builder->getForm()
+                        ->resolve();
+        $form->save();
+
+        return $form;
+    }
+
+    public function setEntry(EntryContract $entry): SubmitForm
+    {
+        $this->entry = $entry;
+
+        return $this;
+    }
+}
