5f686b63e0c2b1b7dcf102f09594faa054f1a2e0
Ali Selcuk <maselcuk@gmail.com>
2019-10-07 20:27:56 +0300

    Add SubFormField type

---
+++ src/Domains/Resource/Field/Types/SubFormField.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace SuperV\Platform\Domains\Resource\Field\Types;
+
+use Illuminate\Http\Request;
+use SuperV\Platform\Domains\Database\Model\Contracts\EntryContract;
+use SuperV\Platform\Domains\Resource\Field\FieldType;
+use SuperV\Platform\Domains\Resource\Form\Contracts\FormInterface;
+use SuperV\Platform\Domains\Resource\Form\FormData;
+use SuperV\Platform\Support\Composer\Payload;
+
+class SubFormField extends FieldType
+{
+    protected $formData;
+
+    protected $entryId;
+
+    protected function boot()
+    {
+        $this->field->on('form.composing', $this->composer());
+    }
+
+    public function saving(FormInterface $form)
+    {
+//        sv_debug('saving', $this->formData);
+    }
+
+    public function resolveValueFromRequest(Request $request, ?EntryContract $entry = null)
+    {
+        $this->formData = $request->get($this->getColumnName());
+
+        return null;
+    }
+
+    public function resolveDataFromRequest(FormData $data, Request $request, ?EntryContract $entry = null)
+    {
+        return parent::resolveDataFromRequest($data, $request, $entry);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFormData()
+    {
+        return $this->formData;
+    }
+
+    public function setEntryId($entryId)
+    {
+        $this->entryId = $entryId;
+
+        return $this;
+    }
+
+    protected function composer()
+    {
+        return function (Payload $payload) {
+            $formUrl = $this->getConfigValue('form');
+            if ($this->entryId) {
+                $formUrl .= '/'.$this->entryId;
+            }
+            $payload->set('config.form', $formUrl);
+            $payload->set('meta.full', true);
+        };
+    }
+}
