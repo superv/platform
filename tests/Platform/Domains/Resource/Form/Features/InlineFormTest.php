ff0b453efa2ab977e4c055597f0144f1143980de
Ali Selcuk <maselcuk@gmail.com>
2019-10-09 05:13:24 +0300

    Add basic tests for Inline Forms feature

---
+++ tests/Platform/Domains/Resource/Form/Features/InlineFormTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Platform\Domains\Resource\Form\Features;
+
+use SuperV\Platform\Domains\Resource\Field\Types\BelongsToField;
+use SuperV\Platform\Domains\Resource\Form\FormFactory;
+use Tests\Platform\Domains\Resource\ResourceTestCase;
+
+class InlineFormTest extends ResourceTestCase
+{
+    function test__add_static_flag_to_field_from_migrations()
+    {
+        $form = FormFactory::builderFromResource($this->blueprints()->clients())
+                           ->getForm();
+
+        $userField = $form->fields()->get('user');
+        $this->assertTrue($userField->hasFlag('static'));
+
+        $form->resolve();
+
+        $this->assertTrue($userField->isHidden());
+
+        $subFormField = $form->fields()->get('user');
+        $this->assertInstanceOf(BelongsToField::class, $subFormField->getConfigValue('parent_type'));
+        $this->assertEquals('platform.users', $subFormField->getConfigValue('resource'));
+        $this->assertEquals($userField->getColumnName(), $subFormField->getColumnName());
+    }
+
+    function test__transform_to_subform_field()
+    {
+        $form = $this->getFormComponent($this->blueprints()->clients());
+
+        $this->assertEquals('sub_form', $form->getField('user', 'type'));
+    }
+}
