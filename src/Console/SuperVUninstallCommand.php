084529a21b9842edd23f586dbddc60dc5b3b7ecc
Ali Selcuk <maselcuk@gmail.com>
2019-10-29 06:44:39 +0300

    Use EnvFile class

--- src/Console/SuperVUninstallCommand.php
+++ src/Console/SuperVUninstallCommand.php
@@ -3,6 +3,7 @@
 namespace SuperV\Platform\Console;
 
 use Illuminate\Console\Command;
+use SuperV\Platform\Console\Jobs\EnvFile;
 use SuperV\Platform\Domains\Addon\Addon;
 use SuperV\Platform\Domains\Addon\AddonCollection;
 use SuperV\Platform\Domains\Addon\Jobs\UninstallAddonJob;
@@ -28,63 +29,12 @@ class SuperVUninstallCommand extends Command
 
         $this->call('migrate:rollback', ['--namespace' => 'platform', '--force' => true]);
 
-        $this->setEnv('SV_INSTALLED=false');
+        EnvFile::load(base_path('.env'))
+               ->set('SV_INSTALLED', 'false')
+               ->write();
 
         PlatformBlueprints::dropTables();
 
         $this->comment("SuperV Uninstalled..! \n");
-    }
-
-    public function setEnv($line)
-    {
-        list($variable, $value) = explode('=', $line, 2);
-
-        $data = $this->readEnvironmentFile();
-
-        array_set($data, $variable, $value);
-
-        $this->writeEnvironmentFile($data);
-    }
-
-    protected function readEnvironmentFile()
-    {
-        $data = [];
-
-        $file = base_path('.env');
-
-        if (! file_exists($file)) {
-            return $data;
-        }
-
-        foreach (file($file) as $line) {
-            // Check for # comments.
-            if (starts_with($line, '#')) {
-                $data[] = $line;
-            } elseif ($operator = strpos($line, '=')) {
-                $key = substr($line, 0, $operator);
-                $value = substr($line, $operator + 1);
-
-                $data[$key] = $value;
-            }
-        }
-
-        return $data;
-    }
-
-    protected function writeEnvironmentFile($data)
-    {
-        $contents = '';
-
-        foreach ($data as $key => $value) {
-            if ($key) {
-                $contents .= PHP_EOL.strtoupper($key).'='.$value;
-            } else {
-                $contents .= PHP_EOL.$value;
-            }
-        }
-
-        $file = base_path('.env');
-
-        file_put_contents($file, $contents);
     }
 }
